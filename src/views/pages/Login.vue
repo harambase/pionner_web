fc14c4943f34f3854a11f41653f8a506d13e9883
s1lin <Myster1234>
2018-06-03 21:00:45 +0800

    删除不要的flag.

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class='app flex-row align-items-center'
-       style="background-image: url('/static/img/download.jpg'); background-size: cover;">
+  <div class='app flex-row align-items-center'>
     <div class='container'>
       <b-row class='justify-content-center'>
         <b-col md='8' style="margin-top: -200px;">
@@ -14,14 +13,16 @@
                   <p class='text-danger'>用户名或者密码不正确!</p>
                 </div>
                 <b-input-group class='mb-4'>
-                  <div class='input-group-prepend'><span class='input-group-text'><i class='icon-user'></i></span></div>
+                  <div class='input-group-prepend'><span class='input-group-text'><i class='icon-user'></i></span>
+                  </div>
                   <input type='text' class='form-control' placeholder='用户ID' v-model='user.username'
                          v-validate="'required|numeric|min:10|max:10'" name="username"
                          :class="{'form-control': true, 'is-invalid': errors.has('username')}">
                   <div v-show="errors.has('username')" class="invalid-tooltip">{{ errors.first('username') }}</div>
                 </b-input-group>
                 <b-input-group class='mb-4'>
-                  <div class='input-group-prepend'><span class='input-group-text'><i class='icon-lock'></i></span></div>
+                  <div class='input-group-prepend'><span class='input-group-text'><i class='icon-lock'></i></span>
+                  </div>
                   <input type='password' class='form-control' placeholder='密码' v-model='user.password'
                          v-validate="'required|min:6'" name="password"
                          :class="{'form-control': true, 'is-invalid': errors.has('password')}">
@@ -32,7 +33,8 @@
                     <b-button variant='primary' class='px-4' @click="doLogin">登录</b-button>
                   </b-col>
                   <b-col cols='4' class='text-right'>
-                    <b-button variant='success' class='px-4 d-md-down' style="display: none;" @click="goToReg">注册</b-button>
+                    <b-button variant='success' class='px-4 d-md-down' style="display: none;" @click="goToReg">注册
+                    </b-button>
                   </b-col>
                   <b-col cols='4' class='text-right'>
                     <b-button variant='link' class='px-0' @click="resetPassword">忘记密码?</b-button>
@@ -54,10 +56,28 @@
           </b-card-group>
         </b-col>
       </b-row>
-
     </div>
   </div>
+
 </template>
+
+<style>
+  .app.flex-row.align-items-center {
+    animation-duration: 2s;
+    background-image: url("/static/img/1.jpg");
+    background-size: cover;
+  }
+
+  .el-carousel__item:nth-child(2) {
+    background-image: url("/static/img/2.jpg");
+    background-size: cover;
+  }
+
+  .el-carousel__item:nth-child(3) {
+    background-image: url("/static/img/3.jpg");
+    background-size: cover;
+  }
+</style>
 
 <script>
   import axios from 'axios'
@@ -71,7 +91,12 @@
           username: '',
           password: ''
         },
-        loginError: false
+        loginError: false,
+        backgroudItems: [
+          '/static/img/1.jpg',
+          '/static/img/2.jpg',
+          '/static/img/3.jpg'
+        ]
       }
     },
     methods: {
