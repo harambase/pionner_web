42dea8b68f3004810de8409440d11f9086a61a44
linsh <lin.shilei@outlook.com>
2018-09-13 22:58:09 +0800

    选课的BUG修改, 主页再调整

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -37,203 +37,201 @@
         </b-card>
       </b-col>
     </b-row>
-    <el-tabs type="border-card" v-model="activeName">
-      <el-tab-pane label="课程表" name="first">
-        <b-card
-          header-tag="header"
-          footer-tag="footer">
-          <b-row>
-            <b-col md="2"></b-col>
-            <b-col md="8">
-              <img :src="basePath + '/static/img/先锋2018年秋季试听课表.jpg'" width="100%"/>
-            </b-col>
-            <b-col md="2">
-              <b-row>
-                <b-button variant="success" @click="downloadSchedule()">下载课程表 Download Schedule</b-button>
-                <!--<b-button>上传课程表</b-button>-->
-              </b-row>
-            </b-col>
-          </b-row>
-        </b-card>
-      </el-tab-pane>
-      <el-tab-pane label="我的课程" name="second">
-        <b-card
-          header-tag="header"
-          footer-tag="footer">
-          <div class="firstStep clearfix" v-for="course in courseList">
-            <div class="lessons-item js-lessons-item g-clearfix">
-              <a>
-                <div class="lessons-pic">
-                  <img v-if="isNotEmpty(course.profile)"
-                       :src="basePath + '/static' + JSON.parse(course.profile).path"
-                       style="width: 200px; height: 200px" class="profile">
-                  <img v-else
-                       :src="basePath + '/static/img/logo.png'"
-                       style="width: 200px; height: 200px" class="profile">
-                </div>
-                <div class="lessons-info">
-                  <h3 class="info-tile">{{course.name}}
-                    ({{course.level}}/{{course.section}})</h3>
-
-                  <div class="lessons-live">
-                    <span class="space">课程周期: {{course.date}}</span>
-                    <span class="space">课程时间：{{course.time}}</span>
-                    <span class="space">每周：{{course.day}}</span>
-                  </div>
-                  <div class="lessons-live">
-                    <span>教师：<span class="js-lesson-record">{{course.faculty}}</span></span>
-                  </div>
-                </div>
-                <div class="lessons-operate">
-                  <div class="status being">进行中</div>
-                  <!--<div class="enter-class">进入课程</div>-->
-                </div>
-              </a>
-            </div>
+    <b-row>
+      <b-col lg="5">
+        <b-card>
+          <div slot="header">
+            <i className="fa fa-align-justify"></i><strong>课程表</strong>
+            <small>Schedule</small>
           </div>
-          <div class="firstStep clearfix" v-if="courseList.length === 0">
-            <div class="lessons-item js-lessons-item g-clearfix" style="cursor: default">
-              <div class="lessons-pic">
-                <img width="200" height="200" src="/static/eas/img/logo.png">
-              </div>
-              <div class="lessons-info">
-                <h3 class="info-tile">无进行中的课程。</h3>
-              </div>
-            </div>
-          </div>
+          <b-card-body>
+            <img :src="basePath + '/static/img/先锋2018年秋季试听课表.jpg'" @click="downloadSchedule()" style="width: 100%; cursor: pointer"/>
+          </b-card-body>
         </b-card>
+      </b-col>
+      <b-col lg="7">
+        <el-tabs type="border-card" v-model="activeName">
+          <el-tab-pane label="我的课程" name="second">
+            <b-card
+              header-tag="header"
+              footer-tag="footer">
+              <div class="firstStep clearfix" v-for="course in courseList">
+                <div class="lessons-item g-clearfix">
+                  <a>
+                    <div class="lessons-pic">
+                      <img v-if="isNotEmpty(course.profile)"
+                           :src="basePath + '/static' + JSON.parse(course.profile).path"
+                           style="width: 150px; height: 150px" class="profile">
+                      <img v-else
+                           :src="basePath + '/static/img/logo.png'"
+                           style="width: 200px; height: 200px" class="profile">
+                    </div>
+                    <div class="lessons-info">
+                      <h3 class="info-tile">{{course.name}}
+                        ({{course.level}}/{{course.section}})</h3>
 
-      </el-tab-pane>
-      <el-tab-pane label="我教的课" name="third" v-if="(rolList.indexOf('1') || rolList.indexOf('6')) !== -1">
-        <b-card
-          header-tag="header"
-          footer-tag="footer">
-          <div class="firstStep clearfix" v-for="course in teachList">
-            <div class="lessons-item js-lessons-item g-clearfix">
-              <a>
-                <div class="lessons-pic">
-                  <img width="200" height="200" src="/static/eas/img/logo.png">
+                      <div class="lessons-live">
+                        <span class="space">课程周期: {{course.date}}</span>
+                        <span class="space">课程时间：{{course.time}}</span>
+                        <span class="space">每周：{{course.day}}</span>
+                      </div>
+                      <div class="lessons-live">
+                        <span>教师：<span class="js-lesson-record">{{course.faculty}}</span></span>
+                      </div>
+                    </div>
+                    <div class="lessons-operate">
+                      <div class="status being">进行中</div>
+                      <!--<div class="enter-class">进入课程</div>-->
+                    </div>
+                  </a>
                 </div>
-                <div class="lessons-info">
-                  <h3 class="info-tile">{{course.name}}
-                    ({{course.level}}/{{course.section}})</h3>
-
-                  <div class="lessons-live">
-                    <span class="space">课程周期: {{course.date}}</span>
-                    <span class="space">课程时间：{{course.time}}</span>
-                    <span class="space">每周：{{course.day}}</span>
+              </div>
+              <div class="firstStep clearfix" v-if="courseList.length === 0">
+                <div class="lessons-item g-clearfix" style="cursor: default">
+                  <div class="lessons-pic">
+                    <img width="200" height="200" src="/static/eas/img/logo.png">
                   </div>
-                  <div class="lessons-complete">
+                  <div class="lessons-info">
+                    <h3 class="info-tile">无进行中的课程。</h3>
+                  </div>
+                </div>
+              </div>
+            </b-card>
+
+          </el-tab-pane>
+          <el-tab-pane label="我教的课" name="third" v-if="(rolList.indexOf('1') || rolList.indexOf('6')) !== -1">
+            <b-card
+              header-tag="header"
+              footer-tag="footer">
+              <div class="firstStep clearfix" v-for="course in teachList">
+                <div class="lessons-item  g-clearfix">
+                  <a>
+                    <div class="lessons-pic">
+                      <img width="200" height="200" src="/static/eas/img/logo.png">
+                    </div>
+                    <div class="lessons-info">
+                      <h3 class="info-tile">{{course.name}}
+                        ({{course.level}}/{{course.section}})</h3>
+
+                      <div class="lessons-live">
+                        <span class="space">课程周期: {{course.date}}</span>
+                        <span class="space">课程时间：{{course.time}}</span>
+                        <span class="space">每周：{{course.day}}</span>
+                      </div>
+                      <div class="lessons-complete">
                     <span class="space">学生人数：
                       <span
                         class="js-lesson-complete">{{course.capacity - course.remain}}</span>
                     </span>
-                    <span class="space">剩余：<span class="js-lesson-rate">{{course.remain}}</span></span>
-                    <span>教室：<span class="js-lesson-record">{{course.classroom}}</span></span>
+                        <span class="space">剩余：<span class="js-lesson-rate">{{course.remain}}</span></span>
+                        <span>教室：<span class="js-lesson-record">{{course.classroom}}</span></span>
+                      </div>
+                    </div>
+                    <div class="lessons-operate">
+                      <div class="status being" v-if="course.status === 0">进行中</div>
+                      <div class="status before" v-if="course.status === 1">未开始</div>
+                      <div class="enter-class">进入课程</div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div class="firstStep clearfix" v-if="teachList.length === 0">
+                <div class="lessons-item  g-clearfix" style="cursor: default">
+                  <div class="lessons-pic">
+                    <img width="200" height="200" src="/static/eas/img/logo.png">
+                  </div>
+                  <div class="lessons-info">
+                    <h3 class="info-tile">无教授的课程。</h3>
                   </div>
                 </div>
-                <div class="lessons-operate">
-                  <div class="status being" v-if="course.status === 0">进行中</div>
-                  <div class="status before" v-if="course.status === 1">未开始</div>
-                  <div class="enter-class">进入课程</div>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div class="firstStep clearfix" v-if="teachList.length === 0">
-            <div class="lessons-item js-lessons-item g-clearfix" style="cursor: default">
-              <div class="lessons-pic">
-                <img width="200" height="200" src="/static/eas/img/logo.png">
               </div>
-              <div class="lessons-info">
-                <h3 class="info-tile">无教授的课程。</h3>
+            </b-card>
+          </el-tab-pane>
+          <el-tab-pane label="你的导师信息" name="forth" v-if="rolList.indexOf('5') !== -1">
+            <b-card header-tag="header"
+                    footer-tag="footer">
+              <div slot="header">
+                <i className="fa fa-align-justify"></i><strong>导师信息</strong>
               </div>
-            </div>
-          </div>
-        </b-card>
-      </el-tab-pane>
-      <el-tab-pane label="你的导师信息" name="forth" v-if="rolList.indexOf('5') !== -1">
-        <b-card header-tag="header"
-                footer-tag="footer">
-          <div slot="header">
-            <i className="fa fa-align-justify"></i><strong>导师信息</strong>
-          </div>
-          <b-row v-if="isNotEmpty(advisor)">
-            <b-col md="9">
-              <b-row>
-                <b-col md="3" class="my-1">
-                  <label class="col-sm-12 control-label">姓名:</label>
+              <b-row v-if="isNotEmpty(advisor)">
+                <b-col md="9">
+                  <b-row>
+                    <b-col md="3" class="my-1">
+                      <label class="col-sm-12 control-label">姓名:</label>
+                    </b-col>
+                    <b-col md="3" class="my-1">
+                      {{advisor.lastName}},{{advisor.firstName}}
+                    </b-col>
+                  </b-row>
+                  <b-row>
+                    <b-col md="3" class="my-1">
+                      <label class="col-sm-12 control-label">生日:</label>
+                    </b-col>
+                    <b-col md="3" class="my-1">
+                      {{advisor.birthday.substr(0,10)}}
+                    </b-col>
+                    <b-col md="3" class="my-1">
+                      <label class="col-sm-12 control-label">邮箱:</label>
+                    </b-col>
+                    <b-col md="3" class="my-1">
+                      {{advisor.email}}
+                    </b-col>
+                  </b-row>
+                  <b-row>
+                    <b-col md="3" class="my-1">
+                      <label class="col-sm-12 control-label">电话号:</label>
+                    </b-col>
+                    <b-col md="3" class="my-1">
+                      {{advisor.tel}}
+                    </b-col>
+                    <b-col md="3" class="my-1">
+                      <label class="col-sm-12 control-label">宿舍号:</label>
+                    </b-col>
+                    <b-col md="3" class="my-1">
+                      {{advisor.dorm}}
+                    </b-col>
+                  </b-row>
+                  <b-row>
+                    <b-col md="3" class="my-1">
+                      <label class="col-sm-12 control-label">QQ号:</label>
+                    </b-col>
+                    <b-col md="3" class="my-1">
+                      {{advisor.qq}}
+                    </b-col>
+                    <b-col md="3" class="my-1">
+                      <label class="col-sm-12 control-label">微信号:</label>
+                    </b-col>
+                    <b-col md="3" class="my-1">
+                      {{advisor.weChat}}
+                    </b-col>
+                  </b-row>
+                  <b-row>
+                    <b-col md="3" class="my-1">
+                      <label class="col-sm-12 control-label">性别:</label>
+                    </b-col>
+                    <b-col md="3" class="my-1">
+                      {{advisor.gender}}
+                    </b-col>
+                  </b-row>
                 </b-col>
-                <b-col md="3" class="my-1">
-                  {{advisor.lastName}},{{advisor.firstName}}
+                <b-col md="3">
+                  <img v-if="isNotEmpty(advisor.profile)"
+                       :src="basePath + '/static' + JSON.parse(advisor.profile).path"
+                       style="width: 200px; height: 200px" class="profile">
+                  <img v-else
+                       :src="basePath + '/static/img/logo.png'"
+                       style="width: 200px; height: 200px" class="profile">
                 </b-col>
               </b-row>
-              <b-row>
-                <b-col md="3" class="my-1">
-                  <label class="col-sm-12 control-label">生日:</label>
-                </b-col>
-                <b-col md="3" class="my-1">
-                  {{advisor.birthday.substr(0,10)}}
-                </b-col>
-                <b-col md="3" class="my-1">
-                  <label class="col-sm-12 control-label">邮箱:</label>
-                </b-col>
-                <b-col md="3" class="my-1">
-                  {{advisor.email}}
-                </b-col>
+              <b-row v-else>
+                <p><strong>暂无导师信息。</strong></p>
               </b-row>
-              <b-row>
-                <b-col md="3" class="my-1">
-                  <label class="col-sm-12 control-label">电话号:</label>
-                </b-col>
-                <b-col md="3" class="my-1">
-                  {{advisor.tel}}
-                </b-col>
-                <b-col md="3" class="my-1">
-                  <label class="col-sm-12 control-label">宿舍号:</label>
-                </b-col>
-                <b-col md="3" class="my-1">
-                  {{advisor.dorm}}
-                </b-col>
-              </b-row>
-              <b-row>
-                <b-col md="3" class="my-1">
-                  <label class="col-sm-12 control-label">QQ号:</label>
-                </b-col>
-                <b-col md="3" class="my-1">
-                  {{advisor.qq}}
-                </b-col>
-                <b-col md="3" class="my-1">
-                  <label class="col-sm-12 control-label">微信号:</label>
-                </b-col>
-                <b-col md="3" class="my-1">
-                  {{advisor.weChat}}
-                </b-col>
-              </b-row>
-              <b-row>
-                <b-col md="3" class="my-1">
-                  <label class="col-sm-12 control-label">性别:</label>
-                </b-col>
-                <b-col md="3" class="my-1">
-                  {{advisor.gender}}
-                </b-col>
-              </b-row>
-            </b-col>
-            <b-col md="3">
-              <img v-if="isNotEmpty(advisor.profile)"
-                   :src="basePath + '/static' + JSON.parse(advisor.profile).path"
-                   style="width: 200px; height: 200px" class="profile">
-              <img v-else
-                   :src="basePath + '/static/img/logo.png'"
-                   style="width: 200px; height: 200px" class="profile">
-            </b-col>
-          </b-row>
-          <b-row v-else>
-            <p><strong>暂无导师信息。</strong></p>
-          </b-row>
-        </b-card>
-      </el-tab-pane>
-    </el-tabs>
+            </b-card>
+          </el-tab-pane>
+        </el-tabs>
+      </b-col>
+    </b-row>
+
   </div>
 </template>
 
@@ -253,7 +251,7 @@
         numOfAdvisor: 0,
         numOfStudent: 0,
         basePath: basePath,
-        activeName: 'first',
+        activeName: 'second',
         advisor: '',
       }
     },
